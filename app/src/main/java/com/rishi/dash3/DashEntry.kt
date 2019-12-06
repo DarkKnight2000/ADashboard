bf04842e2a3cf33f55e3b209af91098acebe9eba
Rishi <rishikaley@gmail.com>
2019-12-06 21:52:16 +0530

    Added dark theme and realm

---
+++ app/src/main/java/com/rishi/dash3/DashEntry.kt
@@ -0,0 +1,19 @@
+package com.rishi.dash3
+
+import io.realm.Realm.setDefaultConfiguration
+import io.realm.RealmConfiguration
+import android.app.Application
+import io.realm.Realm
+
+
+class DashEntry : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+            .name("dash.realm")
+            .schemaVersion(0)
+            .build()
+        Realm.setDefaultConfiguration(realmConfig)
+    }
+}
